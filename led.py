deae153fde8c5662eda7e2f2433033b899d26a3e
Nolan <kurylon@telus.net>
2020-01-10 21:03:49 -0800

    changes

--- led.py
+++ led.py
@@ -39,7 +39,7 @@ def rainbow_cycle(wait):
 
     for j in range(255):
         for i in range(num_pixels):
-            pixel_index = (i * 255 / num_pixels) + j
+            pixel_index = int((i * 256 // num_pixels) + j)
             pixels[i] = wheel(pixel_index & 255)
             pixels.show()
             time.sleep(wait)
