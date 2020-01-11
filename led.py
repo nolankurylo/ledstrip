c53443861597e1a71bdb18a8aefeeec10e73dd28
Nolan <kurylon@telus.net>
2020-01-10 21:33:01 -0800

    changed wheel func

--- led.py
+++ led.py
@@ -9,29 +9,16 @@ pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=1, auto_write=False
                            pixel_order=ORDER)
 
 
-def wheel(pos):
+def wheel(offset):
 
-
-    # Input a value 0 to 255 to get a color value.
-    # The colours are a transition r - g - b - back to r.
-    if pos < 0 or pos > 255:
-        r = g = b = 0
-    elif pos < 85:
-        r = int(pos * 3)
-        g = int(255 - pos*3)
-        b = 0
-    elif pos < 170:
-        pos -= 85
-        r = int(255 - pos*3)
-        g = 0
-        b = int(pos*3)
-    else:
-        pos -= 170
-        pos -= 170
-        r = 0
-        g = int(pos*3)
-        b = int(255 - pos*3)
-    return (r, g, b) 
+    offset = 255 - offset
+    if offset < 85:
+        return (255 - offset * 3, 0, offset * 3)
+    if offset < 170:
+        offset -= 85
+        return (0, offset * 3, 255 - offset * 3)
+    offset -= 170
+    return (offset * 3, 255 - offset * 3, 0)
 
 
 def rainbow_cycle(wait):
