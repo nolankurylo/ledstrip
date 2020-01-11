d7119b17acc95e3ac075f4cb44966cff56ef6e7e
Nolan <kurylon@telus.net>
2020-01-10 20:24:14 -0800

    edited num pixels

--- led.py
+++ led.py
@@ -7,7 +7,7 @@ import neopixel
 # On a Raspberry pi, use this instead, not all pins are supported
 pixel_pin = board.D18
 # The number of NeoPixels
-num_pixels = 10
+num_pixels = 150
 # The order of the pixel colors - RGB or GRB. Some NeoPixels have red and green reversed!
 # For RGBW NeoPixels, simply change the ORDER to RGBW or GRBW.
 ORDER = neopixel.GRB
