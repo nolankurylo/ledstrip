732449965d3187551943c3decffc6c3d7543f8e2
Nolan <kurylon@telus.net>
2020-01-10 21:58:12 -0800

    removed loop

--- led.py
+++ led.py
@@ -21,17 +21,14 @@ def wheel(offset):
 
 
 def rainbow_cycle(wait):
-    for j in range(256):
-        for i in range(num_pixels):
-            pixel_index = (i * 256 // num_pixels) + j
-            pixels[i] = wheel(pixel_index & 255)
-            pixels.show()
-            time.sleep(wait)
-        print("j= "+ str(j))
-    print("end loops")
+    for i in range(num_pixels):
+        pixel_index = (i * 256 // num_pixels) + j
+        pixels[i] = wheel(pixel_index & 255)
+        pixels.show()
+        time.sleep(wait)
+
     return
 while True:
-    print("Loop")
     pixels.fill((255, 0, 0)) # Red
     pixels.show()
     time.sleep(1)
@@ -42,4 +39,3 @@ while True:
     pixels.show()
     time.sleep(1)
     rainbow_cycle(0.001) 
-    print("after")
