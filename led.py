bc7699f1c48dbe025c457207b9da557ef497fa73
Nolan <kurylon@telus.net>
2020-01-14 20:18:47 -0800

    implementing flask

--- led.py
+++ led.py
@@ -1,14 +1,18 @@
 import time
 import board
 import neopixel
+import flask
 
 pixel_pin = board.D18
 num_pixels = 300
 ORDER = neopixel.GRB
 pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=1, auto_write=False,
                            pixel_order=ORDER)
+break_bool = False
 
-def wheel(offset):
+app = Flask(__name__)
+
+def rainbow_rgb(offset):
 
     offset = 255 - offset
     if offset < 85:
@@ -20,39 +24,50 @@ def wheel(offset):
     return (offset * 3, 255 - offset * 3, 0)
 
 
-def rainbow_cycle(wait):
-    for i in range(num_pixels):
-        pixel_index = i * 256 // num_pixels
-        pixels[i] = wheel(pixel_index & 255)
+def rainbow(wait):
+    while True:
+        if break_bool:
+            break_bool = False
+            return
+        pixels.fill((255, 0, 0)) # Red
         pixels.show()
-        time.sleep(wait)
-    return
+        time.sleep(1)
+        pixels.fill((0, 255, 0)) # Green
+        pixels.show()
+        time.sleep(1)
+        pixels.fill((0, 0, 255)) # Blue
+        pixels.show()
+        time.sleep(1)
+        red_cycle(0.000001) 
+        for i in range(num_pixels):
+            pixel_index = i * 256 // num_pixels
+            pixels[i] = rainbow_rgb(pixel_index & 255)
+            pixels.show()
+            time.sleep(wait)
+        
 
-
-def red_cycle(wait):
+def rainbow_single(wait):
     for i in range(num_pixels):
         pixel_index = i * 256 // num_pixels
         pixels.fill((0, 0, 0))
-        pixels[i] = wheel(pixel_index & 255)
+        pixels[i] = rainbow_rgb(pixel_index & 255)
         pixels.show()
         time.sleep(wait)
     
     for i in reversed(range(num_pixels)):
         pixel_index = i * 256 // num_pixels
         pixels.fill((0, 0, 0))
-        pixels[i] = wheel(pixel_index & 255)
+        pixels[i] = rainbow_rgb(pixel_index & 255)
         pixels.show()
         time.sleep(wait)
     return
-while True:
-    # pixels.fill((255, 0, 0)) # Red
-    # pixels.show()
-    # time.sleep(1)
-    # pixels.fill((0, 255, 0)) # Green
-    # pixels.show()
-    # time.sleep(1)
-    # pixels.fill((0, 0, 255)) # Blue
-    # pixels.show()
-    print("hey")
+
+@app.route('/')
+def index():
+    
     time.sleep(1)
-    red_cycle(0.000001) 
+    red_cycle(0.000001)
+    return "index"
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
