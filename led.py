4457f56f7f9d654a6c84588072f1fed4fddd273a
Nolan <kurylon@telus.net>
2020-01-14 21:19:38 -0800

    added bool

--- led.py
+++ led.py
@@ -27,7 +27,9 @@ def rainbow_rgb(offset):
 @app.route('/rainbow')
 def rainbow():
     while True:
-        
+        if break_bool:
+            break_bool = False
+            break
         pixels.fill((255, 0, 0)) # Red
         pixels.show()
         time.sleep(1)
@@ -47,7 +49,9 @@ def rainbow():
 @app.route('/rainbow_single')
 def rainbow_single():
     while True:
-        
+        if break_bool:
+            break_bool = False
+            break
         for i in range(num_pixels):
             pixel_index = i * 256 // num_pixels
             pixels.fill((0, 0, 0))
