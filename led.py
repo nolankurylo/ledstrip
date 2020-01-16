8e95e926cf0655ed7d4e2dba98be851096efd7fd
Nolan <kurylon@telus.net>
2020-01-15 19:21:16 -0800

    as

--- led.py
+++ led.py
@@ -3,11 +3,11 @@ from concurrent.futures import ThreadPoolExecutor
 import board
 import neopixel
 import time
+import asyncio
 
 executor = ThreadPoolExecutor(2)
 
-break_bool1 = False
-break_bool2 = False
+
 
 
 
@@ -36,8 +36,7 @@ def rainbow_rgb(offset):
 
 def background_two():
     print("background 2")
-    global break_bool2
-    while not break_bool2:
+    while True:
         print("in loop 2")
         for i in range(num_pixels):
             pixel_index = i * 256 // num_pixels
@@ -52,14 +51,12 @@ def background_two():
             pixels[i] = rainbow_rgb(pixel_index & 255)
             pixels.show()
             time.sleep(0.01)
-    break_bool2 = False
     return
 
 
 def background_one():
     print("background 1")
-    global break_bool1
-    while not break_bool1:
+    while True:
         print("in loop 2")
         pixels.fill((255, 0, 0))  # Red
         pixels.show()
@@ -75,7 +72,6 @@ def background_one():
             pixels[i] = rainbow_rgb(pixel_index & 255)
             pixels.show()
             time.sleep(0.01)
-    break_bool1 = False
     return
 
 
@@ -86,9 +82,7 @@ def background_one():
 def rainbow():
     reset()
     print("r")
-    global executor, break_bool2
-    break_bool2 = True
-    future = executor.submit(background_one)
+    executor.submit(background_one)
     return ('', 200)
 
 
@@ -97,14 +91,14 @@ def rainbow():
 @app.route('/rainbow_single')
 def rainbow_single():
     reset()
-    print("rs")
-    global executor, break_bool1
-    break_bool1 = True
     future = executor.submit(background_two)
     return ('', 200)
 
 
 def reset():
+    global executor
+    executor.shutdown()
+    executor = ThreadPoolExecutor(2)
     pixels.fill((0, 0, 0))
     return
 
