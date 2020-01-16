d4800f4211828a0818e854c66af2d188bad681b4
Nolan <kurylon@telus.net>
2020-01-15 20:19:19 -0800

    as

--- background_jobs.py
+++ background_jobs.py
@@ -10,23 +10,25 @@ ORDER = neopixel.GRB
 
 pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=1, auto_write=False,
                            pixel_order=ORDER)
-
-while True:
-    print("background 1")
-    print("in loop 1")
-    pixels.fill((255, 0, 0))  # Red
-    pixels.show()
-    time.sleep(1)
-    pixels.fill((0, 255, 0))  # Green
-    pixels.show()
-    time.sleep(1)
-    pixels.fill((0, 0, 255))  # Blue
-    pixels.show()
-    time.sleep(1)
-    for i in range(num_pixels):
-        pixel_index = i * 256 // num_pixels
-        pixels[i] = rainbow_rgb(pixel_index & 255)
+def main():
+    while True:
+        print("background 1")
+        print("in loop 1")
+        pixels.fill((255, 0, 0))  # Red
         pixels.show()
-        time.sleep(0.01)
+        time.sleep(1)
+        pixels.fill((0, 255, 0))  # Green
+        pixels.show()
+        time.sleep(1)
+        pixels.fill((0, 0, 255))  # Blue
+        pixels.show()
+        time.sleep(1)
+        for i in range(num_pixels):
+            pixel_index = i * 256 // num_pixels
+            pixels[i] = rainbow_rgb(pixel_index & 255)
+            pixels.show()
+            time.sleep(0.01)
     
 
+if __name__ == "__main__":
+    main()
