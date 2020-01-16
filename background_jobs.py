0a24b448abc057cec783cf3c29dc80528d3ce7d8
Nolan <kurylon@telus.net>
2020-01-15 20:20:10 -0800

    as

--- background_jobs.py
+++ background_jobs.py
@@ -10,25 +10,35 @@ ORDER = neopixel.GRB
 
 pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=1, auto_write=False,
                            pixel_order=ORDER)
-def main():
-    while True:
-        print("background 1")
-        print("in loop 1")
-        pixels.fill((255, 0, 0))  # Red
+
+
+def rainbow_rgb(offset):
+    
+    offset = 255 - offset
+    if offset < 85:
+        return (255 - offset * 3, 0, offset * 3)
+    if offset < 170:
+        offset -= 85
+        return (0, offset * 3, 255 - offset * 3)
+    offset -= 170
+    return (offset * 3, 255 - offset * 3, 0)
+
+while True:
+    print("background 1")
+    print("in loop 1")
+    pixels.fill((255, 0, 0))  # Red
+    pixels.show()
+    time.sleep(1)
+    pixels.fill((0, 255, 0))  # Green
+    pixels.show()
+    time.sleep(1)
+    pixels.fill((0, 0, 255))  # Blue
+    pixels.show()
+    time.sleep(1)
+    for i in range(num_pixels):
+        pixel_index = i * 256 // num_pixels
+        pixels[i] = rainbow_rgb(pixel_index & 255)
         pixels.show()
-        time.sleep(1)
-        pixels.fill((0, 255, 0))  # Green
-        pixels.show()
-        time.sleep(1)
-        pixels.fill((0, 0, 255))  # Blue
-        pixels.show()
-        time.sleep(1)
-        for i in range(num_pixels):
-            pixel_index = i * 256 // num_pixels
-            pixels[i] = rainbow_rgb(pixel_index & 255)
-            pixels.show()
-            time.sleep(0.01)
+        time.sleep(0.01)
     
 
-if __name__ == "__main__":
-    main()
