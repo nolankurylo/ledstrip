287ab9b59cb1bd20a7b9e81d95e5f1c358d3122b
Nolan <kurylon@telus.net>
2020-01-15 20:18:01 -0800

    as

--- background_jobs.py
+++ background_jobs.py
@@ -1,3 +1,16 @@
+import board
+import neopixel
+import time
+
+
+
+pixel_pin = board.D18
+num_pixels = 300
+ORDER = neopixel.GRB
+
+pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=1, auto_write=False,
+                           pixel_order=ORDER)
+
 while True:
     print("background 1")
     print("in loop 1")
