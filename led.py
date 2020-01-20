4ec5139c1121e231b915dbde6ea6a441d4db57fd
Nolan <kurylon@telus.net>
2020-01-19 23:27:19 -0800

    as

--- led.py
+++ led.py
@@ -16,7 +16,7 @@ pixel_pin = board.D18
 num_pixels = 300
 ORDER = neopixel.GRB
 
-pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=1, auto_write=False,
+pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.1, auto_write=False,
                            pixel_order=ORDER)
 
 
